1f51d4b9f21137b03253933d2a78d328721f3b7f
cooper <cooper.hy.zhang@foxmail.com>
2026-05-21 22:30:00 +0800

    Initial Commit

---
+++ CodeClip/CodeClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeClipApp.swift
+//  CodeClip
+//
+//  Created by eason cooper on 2026/5/21.
+//
+
+import SwiftUI
+
+@main
+struct CodeClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
